689d37edd42a6b9220d6a384cc951a88466947b1
amurillo18 <80285729+amurillo18@users.noreply.github.com>
2023-03-28 14:15:08 -0700

    updating server.js for deployment

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express()
 const morgan = require("morgan")
 const mongoose = require("mongoose")
 require('dotenv').config()
-const connectDB = require('./config/db')
+//const connectDB = require('./config/db')
 const {errorHandler} = require('./middleware/errorMiddleware')
+const path = require("path")
 
 //why i used mvc:  https://www.freecodecamp.org/news/the-model-view-controller-pattern-mvc-architecture-and-frameworks-explained/
 
@@ -13,14 +14,19 @@ const port = process.env.PORT || 4000;
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))//method inbuilt in express to recognize the incoming Request Object as strings or arrays.
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 mongoose.set('strictQuery', true)
-connectDB()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 app.use('/auth', require('./routes/authRouter'))
 app.use('/api/tickets', require('./routes/ticketRouter'))
 
 app.use(errorHandler)
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`the server is listening on port ${port}`)
